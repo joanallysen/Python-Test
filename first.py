b6e0d70a39c1a028edba8e64e8597bf2e39cb700
joanallysen <joanallysen@gmail.com>
2024-10-01 15:49:20 +1300

    this is a new commit

---
+++ first.py
@@ -0,0 +1,13 @@
+class GetAnswer(object):
+    def __init__(self):
+        pass
+
+    def findClosesNumber(self, nums):
+        print("something")
+
+def main():
+    answer = GetAnswer();
+    print("This is a new file, dont mind me")
+
+if __name__ == '__main__':
+    main()
